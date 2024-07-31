2d32746dfe23afbe32470019c23741276f53f2f7
Abdelrahman Abdelnasser Gamal <2021170298@cis.asu.edu.eg>
2024-08-01 01:53:43 +0300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -14,8 +14,8 @@ provider "kubernetes" {
   host                   = aws_eks_cluster.library_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.library_cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-  config_path            = "~/.kube/config"
-  config_context         = "arn:aws:eks:us-west-2:637423483309:cluster/team3-library-cluster"
+  # config_path            = "~/.kube/config"
+  # config_context         = "arn:aws:eks:us-west-2:637423483309:cluster/team3-library-cluster"
 }
 
 
